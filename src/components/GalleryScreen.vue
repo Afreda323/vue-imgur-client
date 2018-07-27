9f7b4a4810656782d3f73f5c1e8480d939678b61
Afreda323 <anthonyfreda323@gmail.com>
2018-07-27 11:50:24 -0400

    finished upload/gallery

--- src/components/GalleryScreen.vue
+++ src/components/GalleryScreen.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container class="container" v-if="isLoggedIn">
     <v-layout v-if="loadingImages" fill-height justify-center align-center>
       <v-progress-circular 
         indeterminate 
@@ -7,7 +7,7 @@
       />
     </v-layout>
     <h1 v-if="noImages">0 Images Found</h1>
-    <v-card v-else v-for="image in allImages">
+    <v-card v-else v-for="image in allImages" class="card-wrap">
       <v-card-media
         :src="image.link"
         contain
@@ -16,6 +16,9 @@
       </v-card-media>
     </v-card>
   </v-container>
+  <v-container v-else>
+    <h1>Log in to get started</h1>
+  </v-container>
 </template>
 
 <script>
@@ -23,9 +26,9 @@ import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: "GalleryScreen",
-  methods: mapActions(['fetchImages']),
+  methods: mapActions(['fetchImages',]),
   computed: {
-    ...mapGetters(['allImages', 'loadingImages']),
+    ...mapGetters(['allImages', 'loadingImages', 'isLoggedIn']),
     noImages() {
       return !this.loadingImages && this.allImages.length < 1
     },
@@ -35,3 +38,27 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+  .container {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+  .card-wrap {
+    padding: 10px 20px;
+    margin: 2px 0;
+    width: 33%;
+  }
+  @media (max-width: 900px) {
+    .card-wrap {
+      width: 49%;
+    }
+  }
+  @media (max-width: 600px) {
+    .card-wrap {
+      width: 100%;
+    }
+  }
+</style>
